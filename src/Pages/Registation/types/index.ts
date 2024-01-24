fb3e2283429de987f6113e368308d4b8a425a7bd
Dency675 <neelamkavil2002@gmail.com>
2024-01-24 17:19:05 +0530

    FC

---
+++ src/Pages/Registation/types/index.ts
@@ -0,0 +1,16 @@
+import { MouseEvent } from "react";
+
+export type RegisterFunction = (e: React.FormEvent) => void;
+
+// Props interface for the RegistrationPage component
+export interface RegistrationPageProps {
+  register: RegisterFunction;
+}
+
+export interface FormInput {
+  full_name: string | null;
+  e_mail: string | null;
+  password: string | null;
+  client_type: "supplier" | "customer" | null;
+  profile_pic: File | null;
+}
